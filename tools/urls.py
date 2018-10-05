3705b6851ccf030b40693dbe60f75da5ed26a26d
Patrick Forringer <patrick@forringer.com>
2018-10-05 00:07:45 -0500

    format tools urls

--- tools/urls.py
+++ tools/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 
 from tools.views import CreateUserTool, UserToolList
 
-app_name = 'tools'  #url namespace
+app_name = "tools"  # url namespace
 
 urlpatterns = [
-    path('', UserToolList.as_view(), name='home'),
-    path('create/', CreateUserTool.as_view(), name='create'),
+    path("", UserToolList.as_view(), name="home"),
+    path("create/", CreateUserTool.as_view(), name="create"),
 ]
-
