c5c93f1011bc0a372175b3d113e6693095e490a8
Patrick Forringer <patrick@forringer.com>
2018-10-21 23:43:14 -0500

    Start of admin toolbar on tool detail page

--- toolhub/urls.py
+++ toolhub/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from django.views.generic import TemplateView
+
 import toolhub.views as toolhub_views
 
 
--- tools/urls.py
+++ tools/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path("<int:pk>/history", UserToolHistoryView.as_view(), name="history"),
     path("<int:pk>/edit", UserToolUpdateView.as_view(), name="edit"),
     path("<int:pk>/clear", ClearUserView.as_view(), name="clear"),
+    path("<int:pk>/delete", ClearUserView.as_view(), name="delete"),
     path("create/", UserToolCreateView.as_view(), name="create"),
 ]
