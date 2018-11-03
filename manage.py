b0c23ea53d81ffacaa5273e382872504aacaeea4
vkdvamshi <43324526+vkdvamshi@users.noreply.github.com>
2018-11-02 21:30:22 -0500

    Update manage.py
    Errors: C:\Users\home-PC\source\repos\toolhub>docker-compose up
    Creating network "toolhub-net" with driver "bridge"
    Creating toolhub_db_1 ... done
    Creating toolhub_web_1 ... done
    Attaching to toolhub_db_1, toolhub_web_1
    db_1   | 2018-11-03 02:17:19.034 UTC [1] LOG:  listening on IPv4 address "0.0.0.0", port 5432
    db_1   | 2018-11-03 02:17:19.034 UTC [1] LOG:  listening on IPv6 address "::", port 5432
    db_1   | 2018-11-03 02:17:19.045 UTC [1] LOG:  listening on Unix socket "/var/run/postgresql/.s.PGSQL.5432"
    db_1   | 2018-11-03 02:17:19.077 UTC [21] LOG:  database system was shut down at 2018-11-03 02:16:47 UTC
    db_1   | 2018-11-03 02:17:19.093 UTC [1] LOG:  database system is ready to accept connections
    web_1  |   File "./manage.py", line 14
    web_1  |     ) from exc
    web_1  |          ^
    web_1  | SyntaxError: invalid syntax
    toolhub_web_1 exited with code 1
    web_1  |   File "./manage.py", line 14
    web_1  |     ) from exc
    web_1  |          ^
    
    
    Fix : formatted file correctly to avoid issues

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 import os
 import sys
 
@@ -7,9 +7,8 @@ if __name__ == "__main__":
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
+        raise ImportError("Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+            "forget to activate a virtual environment?") from exc
     execute_from_command_line(sys.argv)
+    print("Process manage.py Completed")
