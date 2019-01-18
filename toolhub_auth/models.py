14b14cd972fb338402537a77f63ab777e6748a6a
Patrick Forringer <patrick@forringer.com>
2019-01-18 17:45:54 -0600

    Add user profile link to pages
    + move badges.jinja to partials dir

--- toolhub_auth/models.py
+++ toolhub_auth/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from django_extensions.db.models import TimeStampedModel
 from markdownx.models import MarkdownxField
