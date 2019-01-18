0f3e77058f3c7ad50c73c3a830927fe474b0baf9
Patrick Forringer <patrick@forringer.com>
2019-01-18 17:33:00 -0600

    gravatar.py cleanup and temp default image

--- utils/templatetags/gravatar.py
+++ utils/templatetags/gravatar.py
@@ -1,21 +1,19 @@
 import hashlib
 import urllib
 from django_jinja import library, utils
-import jinja2
 
 
 @library.global_function
-def gravatar_url(email, size=40):
+def gravatar_url(email=None, size=40):
     """
     return only the URL of the gravatar
     TEMPLATE USE:  {{ gravatar_url(email, 150) }}
     """
-    assert email
     # TODO: change default image
-    default = "https://example.com/static/images/defaultavatar.jpg"
-    email = email.encode('utf-8')
+    default = "https://www.ucas.com/sites/default/files/default_images/default_avatar.png"
+    email = email.encode("utf-8")
     md5_hash = hashlib.md5(email.lower()).hexdigest()
-    params = urllib.parse.urlencode({'d':default, 's':str(size)})
+    params = urllib.parse.urlencode({"d": default, "s": str(size)})
     return f"https://www.gravatar.com/avatar/{md5_hash}?{params}"
 
 
