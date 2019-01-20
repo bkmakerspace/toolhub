38ccaa76ca8d745de1893dd4b14aa6452eb579ae
Patrick Forringer <patrick@forringer.com>
2019-01-20 15:46:23 -0600

    black formatting on tests

--- toolhub_auth/tests.py
+++ toolhub_auth/tests.py
@@ -4,6 +4,6 @@ from model_mommy import mommy
 
 class TestUser(TestCase):
     def test_profile_created_on_inital_save(self):
-        user = mommy.make('toolhub_auth.User', first_name='Test', last_name='User')
+        user = mommy.make("toolhub_auth.User", first_name="Test", last_name="User")
 
-        self.assertEqual(str(user.profile), 'Test User profile')
+        self.assertEqual(str(user.profile), "Test User profile")
