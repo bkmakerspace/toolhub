a19a2ca150f794eb429b7b9cd487a27794e4cff1
Patrick Forringer <patrick@forringer.com>
2019-03-09 18:47:34 -0600

    change todo to notice

--- toolhub/settings/prod.py
+++ toolhub/settings/prod.py
@@ -17,7 +17,7 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
-# TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+# NOTICE: set this to 60 seconds first and then to 518400 once you prove the former works
 SECURE_HSTS_SECONDS = env.int("DJANGO_SECURE_HSTS_SECONDS", default=60)
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True)
